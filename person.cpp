6480aa08aaf5dddc6aa70d76533ded8a5cf87a66
GuanlinWang <43226744+GuanlinWang@users.noreply.github.com>
2019-02-21 20:00:17 -0800

    Update person.cpp

--- person.cpp
+++ person.cpp
@@ -1,1 +1,48 @@
+#include "person.h"
+
+
+string Person::getFirstName()
+{
+    return firstName;
+}
+string Person::getLastName()
+{
+    return lastName;
+}
+float Person::getPayRate()
+{
+    return payRate;
+}
+float Person:: getHoursWorked()
+{
+    return hoursWorked;
+}
+void Person:: setFirstName(string fName)
+{
+    firstName=fName;
+}
+void Person:: setLastName(string lName)
+{
+    lastName=lName;
+}
+
+void Person:: setPayRate(float rate)
+{
+    payRate=rate;
+}
+void Person:: setHoursWorked(float hours)
+{
+    hoursWorked=hours;
+}
+
+string Person:: fullName()
+{
+    string fullName=lastName+" "+firstName;
+    return fullName;
+}
+float Person:: totalPay()
+{
+    float total=payRate*hoursWorked;
+    return total;
+}
 

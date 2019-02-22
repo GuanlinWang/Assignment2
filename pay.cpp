fe086e3dcb1833c51775c6ae2d14a51ac3526324
GuanlinWang <43226744+GuanlinWang@users.noreply.github.com>
2019-02-21 21:26:00 -0800

    Update pay.cpp

--- pay.cpp
+++ pay.cpp
@@ -2,17 +2,47 @@
 #include <iostream>
 #include <fstream>
 
-
-void raedData(Person input[]);
+void writeData(Person output[],int size);
+void readData(Person input[],int &size);
 int main()
 {
+  int size;
     Person employee[20];
+    readData(employee,size);
+    writeData(employee,size);
     return 0;
 }
 
-void readData(Person input[])
+void readData(Person input[],int &size)
 {
+  int temp=0;
+  string first,last;
+  float hours, rate;
     ifstream file;
-    file.open("input.txt");
+   file.open("input.txt");
+   for (int i =0; i<20;i++)
+   {
+     file>>last>>first>>rate>>hours;
+     if(file.peek(),file.eof())
+    {break;}
+     input[i].setFirstName(first);
+     input[i].setLastName(last);
+     input[i].setPayRate(rate);
+     input[i].setHoursWorked(hours);
+     temp++;
 
+   }
+   file.close();
+   size=temp;
+}
+
+void writeData(Person output[], int size)
+{
+  fstream file;
+  file.open("output.txt",fstream::out);
+  for (int i=0;i<size;i++)
+  {
+    file<<output[i].fullName()<<" "<<output[i].totalPay()<<endl;
+  }
+  file.close();
 }

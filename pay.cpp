e869c93e24e79dccfb9c7aae4c3001a5ea722fe6
GuanlinWang <43226744+GuanlinWang@users.noreply.github.com>
2019-02-21 20:00:47 -0800

    Create pay.cpp

---
+++ pay.cpp
@@ -0,0 +1,18 @@
+#include "person.cpp"
+#include <iostream>
+#include <fstream>
+
+
+void raedData(Person input[]);
+int main()
+{
+    Person employee[20];
+    return 0;
+}
+
+void readData(Person input[])
+{
+    ifstream file;
+    file.open("input.txt");
+
+}
